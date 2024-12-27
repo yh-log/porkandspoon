871bb5f28bf256257e89244cd9e527f35bf48252
이진형 <wjsaus127@naver.com>
2024-12-27 17:00:36 +0900

    게시글 등록, 조직도 수정 게시글 리스트, 검색 기능 완료

--- src/main/java/kr/co/porkandspoon/dao/BoardDAO.java
+++ src/main/java/kr/co/porkandspoon/dao/BoardDAO.java
@@ -5,19 +5,17 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kr.co.porkandspoon.dto.BoardDTO;
 import kr.co.porkandspoon.dto.FileDTO;
 
 @Mapper
 public interface BoardDAO {
 
-	void setBoardfiles(List<FileDTO> results);
+	int setBoardfiles(FileDTO results);
 
-	int setBoardwrite(Map<String, Object> params);
-	
-	List<Map<String, Object>> getBoardList(int offset, int cnt, String option, String search);
+	int setBoardwrite(BoardDTO dto);
 
-	// 게시글 개수를 가져오는 메서드
-	int getBoardCount(String option, String search);
+	List<BoardDTO> boardList(Map<String, Object> parmeterMap);
     
     
     
